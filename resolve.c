3d13de23bef6796f0f89a5e9961be6972867a025
andyv <andyv>
2002-01-27 05:31:37 +0000

    Really dumb bug in previous patch

--- resolve.c
+++ resolve.c
@@ -65,7 +65,7 @@ g95_symbol *sym;
 
     if (sym == NULL) continue;  /* Alternate return placeholder */
 
-    if (sym->attr.subroutine == FL_PROCEDURE) continue;
+    if (sym->attr.subroutine) continue;
 
     if (sym->ts.type == BT_UNKNOWN) g95_set_default_type(sym, 1, sym->ns);
 
