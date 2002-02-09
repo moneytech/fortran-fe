15df767e549e5fe1dae3e5128aad871540eb0e54
andyv <andyv>
2002-02-09 21:20:32 +0000

    Don't assign default types to external or intrinsic dummy parameters

--- resolve.c
+++ resolve.c
@@ -65,7 +65,8 @@ g95_symbol *sym;
 
     if (sym == NULL) continue;  /* Alternate return placeholder */
 
-    if (sym->attr.subroutine) continue;
+    if (sym->attr.subroutine || sym->attr.external || sym->attr.intrinsic)
+      continue;
 
     if (sym->ts.type == BT_UNKNOWN) g95_set_default_type(sym, 1, sym->ns);
 
