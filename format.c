294e2f849e81f3f7be425f8162b78f54ea66081e
andyv <andyv>
2002-05-21 05:55:56 +0000

    Prevent a repeat count from following the P descriptor.

--- format.c
+++ format.c
@@ -388,6 +388,13 @@ data_desc:
     break;
 
   case FMT_P:
+    t = format_lex();
+    if (t == FMT_POSINT) {
+      error = "Repeat count cannot follow P descriptor";
+      goto syntax;
+    }
+
+    saved_token = t;
     goto optional_comma;
 
   case FMT_POS:
