d7d00842ed5c5e29a0385645c48e04c3b73a134a
andyv <andyv>
2003-01-07 05:35:39 +0000

    Plug memory leak (Arnaud)

--- io.c
+++ io.c
@@ -987,6 +987,8 @@ cleanup:
 static void terminate_io(g95_code *io_code) {
 g95_code *c;
 
+  if (io_code == NULL) return; 
+
   c = g95_get_code();
   c->op = EXEC_DT_END;
   c->ext.dt = new_st.ext.dt;     /* Point to structure that is already there */
